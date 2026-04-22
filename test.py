109a642649dc9d3f64c50985a3d499ceb0d5eed6
engmohmadamer-max <eng.mohmad.amer@gmail.com>
2026-04-22 12:32:27 +0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-store = {
-    'Seafood':{'Small':200, 'Medium':300},
-    'Pasta':{'Small':200, 'Medium':300},
-    'Water':{'Small':200, 'Medium':300}
-}
-store['Steak'] = 200
-
-print(store['Seafood']['Small'])
